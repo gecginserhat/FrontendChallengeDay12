5090a21bd5109099a94540d8ec246128c185d7b1
Serhat GECGIN <74310970+gecginserhat@users.noreply.github.com>
2024-09-07 17:02:31 +0300

    Add files via upload

---
+++ Window/app.js
@@ -0,0 +1,53 @@
+// alert
+
+//alert("Merhaba");
+
+
+//console.log(cevap);
+
+//confirm
+/*
+if (confirm("Are you sure")) {
+    console.log("Delete");
+}else {
+    console.log("Cancel");
+}*/
+
+
+/*
+const cevap = prompt("2 + 2 = ?");
+if (cevap == "4") {
+    console.log("True");
+}else {
+    console.log("False");
+}*/
+
+let value;
+
+value = window;
+value = window.location;
+value = window.location.host;
+value = window.location.hostname;
+value = window.location.port;
+value = window.location.href;
+
+/*
+if (confirm("Do you want to refresh the page?")) {
+    window.location.reload();
+}
+else {
+    console.log("Page refresh canceled");
+}
+console.log(value);
+*/
+
+
+value = window.outerHeight;
+//value = window.outerWidth;
+
+value = window.innerHeight;
+
+//We see how much the scroll has been shifted on the x-axis.
+value = window.scrollX;
+
+console.log(value);
