b5e5265b002fea5ab149f59087c5518b2f8f6757
Serhat GECGIN <74310970+gecginserhat@users.noreply.github.com>
2024-09-07 17:03:05 +0300

    Add files via upload

---
+++ Scope/app.js
@@ -0,0 +1,36 @@
+// Scope concept
+
+//By using let and const, we prevent possible password change errors.
+const database = "123456";
+
+/*
+var value1 = 10;
+let value2 = 20;
+const value3 = 30;
+*/
+
+/* 
+function Func() {
+    var value1 = 50;
+    let value2 = 60;
+    const value3 = 70;
+
+    console.log(value1, value2, value3);
+}
+Func();
+*/
+
+/*
+if (true) {
+    var a = 40;
+    let b = 50;
+    const c = 60;
+    console.log(a, b, c);
+}
+
+
+
+console.log(value1, value2, value3);*/
+if (true) {
+    const database = "34343434";
+}
